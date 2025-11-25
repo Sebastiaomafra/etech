0233af480e2a7f121d27cd0d36de65e5f2328034
crgab <gabrielcruzds.694@gmail.com>
2025-11-25 19:44:13 -0400

    first commit

---
+++ Operadores basicos/tipo basico.py
@@ -0,0 +1,24 @@
+inteiro = 10 # int(Numero inteiro)
+decimal = 3.14159 # Float (Numero decimal)
+complexo = 3+4j # complex (Numero complexo)
+
+print (inteiro, decimal, complexo)
+print (f"Tipos: {type(inteiro)}, {type(decimal)}, {type(complexo)}")
+
+# Texto
+Texto = "Ola mundo!" #str (string / texto)
+print(f"Tipos: {type(Texto)}")
+
+# Booleanos
+verdadeiro = True # bool (booleano verdadeiro)
+falso = False # bool (booleano falso)
+print (verdadeiro, falso)
+print(f"Tipos: {type(verdadeiro)}, {type(falso)}")
+
+#colecoes
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # list (lista mutavel)
+tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) # tupla (tupla imutavel)
+dicionario = {"Nome": "Sebastiao"} # dic (dicionario)
+conjunto = {1, 2, 3, 4} # ser(conjunto)
+print (lista, tupla, dicionario, conjunto)
+print(f"Tipos: {type(lista)}, {type(tupla)}, {type(dicionario)}, {type(conjunto)}")
